f404017172d043c729afb77e8a92e53c2bc80503
AlexKVal <alexkval@gmail.com>
2016-07-07 00:46:40 +0300

    add elm scaffolding

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+// Set up Elm
+Elm.App.embed(document.querySelector('#elm-container'))
